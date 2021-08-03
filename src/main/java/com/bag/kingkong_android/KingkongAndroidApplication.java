4428fe317c49dc76d9d77d86ec87e519824f0ca6
liyong <taihangxingren@163.com>
2021-08-03 16:08:17 +0800

    init

---
+++ src/main/java/com/bag/kingkong_android/KingkongAndroidApplication.java
@@ -0,0 +1,13 @@
+package com.bag.kingkong_android;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingkongAndroidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingkongAndroidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bag/kingkong_android/KingkongAndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bag.kingkong_android;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingkongAndroidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
