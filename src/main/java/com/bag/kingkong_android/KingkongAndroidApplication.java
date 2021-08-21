c075d50450358c933e740aaf502a3c84c332b8e5
liyong <taihangxingren@163.com>
2021-08-21 20:48:18 +0800

    Integrate Nacos

--- src/main/java/com/bag/kingkong_android/KingkongAndroidApplication.java
+++ src/main/java/com/bag/kingkong_android/KingkongAndroidApplication.java
@@ -2,8 +2,12 @@ package com.bag.kingkong_android;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class KingkongAndroidApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/bag/kingkong_android/rest/TestRest.java
+++ src/main/java/com/bag/kingkong_android/rest/TestRest.java
@@ -1,12 +1,14 @@
 package com.bag.kingkong_android.rest;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/")
 @Component
@@ -18,5 +20,13 @@ public class TestRest {
     @Consumes({MediaType.APPLICATION_JSON})
     public String test(String param){
         return param;
+    }
+
+    @Path("feignTest")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    public String upload() {
+        return "Hello Baby";
     }
 }
